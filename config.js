4d7be3a2be13ebf7743173099980a0465d813bd1
Nguyễn Văn Sỹ <78789558+nguyenvansy16@users.noreply.github.com>
2021-07-02 15:14:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
     introTitle: 'Em Sỹ à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    `,
     btnIntro: 'hihi',
     title: 'Chọn con tim hay là nghe lí trí đây...',
     desc: '20 tuổi rồi nghĩ thử xem mình có gì trong tay nhé!',
